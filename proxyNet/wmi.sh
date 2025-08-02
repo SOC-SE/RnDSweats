f91b436c22692a6ad8cf960049d282e9ff495038
SOC-SE <samuelbrucker296@gmail.com>
2025-08-02 18:32:40 -0500

    Update wmi.sh

--- proxyNet/wmi.sh
+++ proxyNet/wmi.sh
@@ -18,6 +18,8 @@
 
 # --- Globals and Utility Functions ---
 LOG_FILE="/var/log/wazuh_universal_installer.log"
+WazuhInstallationVersion="4.12"
+
 
 # Function to print messages to stdout and the log file
 log_msg() {
@@ -126,7 +128,7 @@ install_all_in_one_assisted() {
 
     info "Downloading the Wazuh installation assistant..."
     # Using the latest 4.x branch installer
-    curl -sO https://packages.wazuh.com/4.x/wazuh-install.sh
+    curl -sO curl -sO https://packages.wazuh.com/$WazuhInstallationVersion/wazuh-install.sh && sudo bash ./wazuh-install.sh -a
     check_success
 
     info "Running the All-in-One installation. This may take several minutes..."
