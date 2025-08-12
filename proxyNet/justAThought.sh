29a6a900876259b6249ae81c885bdf009ddb76a0
SOC-SE <samuelbrucker296@gmail.com>
2025-08-11 20:53:46 -0500

    Update justAThought.sh

--- proxyNet/justAThought.sh
+++ proxyNet/justAThought.sh
@@ -42,7 +42,7 @@ gpgcheck=1
 gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
 enabled=1
 name=Wazuh repository
-baseurl=https://packages.wazuh.com/4.x/yum/x86_64
+baseurl=https://packages.wazuh.com/4.x/yum/
 protect=1
 EOF
 
