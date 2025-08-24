ad6641db70a79bfc270dd1c55f60e52145e7cd0a
SOC-SE <samuelbrucker296@gmail.com>
2025-08-24 17:49:12 -0500

    Create yara.sh

---
+++ Tools/Wazuh/activeResponses/yara.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Get the file path from the alert
+FILE_PATH=$(echo "$3" | jq -r .parameters.alert.syscheck.path)
+
+# Yara binary path
+YARA_BIN="/usr/bin/yara"
+
+# Yara rules path
+YARA_RULES="/var/ossec/etc/yara/rules/your_rules.yar" # Change to your rules file or a directory
+
+# Log file for Yara scan results
+LOG_FILE="/var/ossec/logs/active-responses.log"
+
+# Run the Yara scan
+$YARA_BIN -r $YARA_RULES "$FILE_PATH" >> $LOG_FILE
