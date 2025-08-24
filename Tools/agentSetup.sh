ae0e600de70f2d007d1132a058ca5ba8a874f23b
SOC-SE <samuelbrucker296@gmail.com>
2025-08-24 13:32:22 -0500

    Update agentSetup.sh

--- Tools/agentSetup.sh
+++ Tools/agentSetup.sh
@@ -14,25 +14,16 @@ fi
 echo "Setting up auditd"
 bash Auditd/auditdSetup.sh
 
-if systemctl is-active --quiet auditd; then
-    echo "✅ Verification successful! The auditd service is active."
-    echo "🔎 To see the current rules loaded in kernel, run this command:"
-    echo "sudo auditctl -l"
-else
-    echo "❌ Verification failed. The auditd service could not be started."
-    exit 1
-fi
-
+echo "Setting up yara"
+bash Yara/yaraSetup.sh
 
 echo "Setting up the Wazuh agent"
 bash Wazuh/linuxSetup.sh
 
-if systemctl is-active --quiet wazuh-agent; then
-    echo "✅ Verification successful! The Wazuh Agent service is active."
-else
-    echo "❌ Verification failed. The Wazuh Agent service could not be started."
-    exit 1
-fi
 
 
-echo "Everything is set up and all good to go, Stage 1 is complete. Good luck!"
+echo "############################################################################"
+echo "#                                                                          #
+echo "# Everything is set up and all good to go, Stage 1 is complete. Good luck! #"
+echo "#                                                                          #"
+echo "############################################################################"
