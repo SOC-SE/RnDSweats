af683cb2aae9d7e5d63eb31dd4575c7abb618e67
SOC-SE <samuelbrucker296@gmail.com>
2025-09-18 21:31:18 -0500

    Update wazuhInstallationTemp.sh

--- wazuhInstallationTemp.sh
+++ wazuhInstallationTemp.sh
@@ -7,8 +7,8 @@ fi
 
 apt install curl -y
 
-curl -sO https://packages.wazuh.com/4.12/wazuh-install.sh
-curl -so wazuh-passwords-tool.sh https://packages.wazuh.com/4.12/wazuh-passwords-tool.sh
+curl -sO https://packages.wazuh.com/4.13/wazuh-install.sh
+curl -so wazuh-passwords-tool.sh https://packages.wazuh.com/4.13/wazuh-passwords-tool.sh
 
 bash wazuh-install.sh -a
 
