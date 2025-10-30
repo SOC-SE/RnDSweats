4ad894120e3bb793f9e9e16b153459ed5c24d851
Alex Boehne <alex.boehne04@gmail.com>
2025-10-30 15:29:56 -0500

    Stroked out
    genuinely stroked out in the initial commit doing 'cat's instead of 'echo's. also added demarcation to increase readability of config file.

--- Tools/VyOS/vyosEnumerate.sh
+++ Tools/VyOS/vyosEnumerate.sh
@@ -4,22 +4,25 @@ if [ "$(id -g -n)" != 'vyattacfg' ] ; then
     exec sg vyattacfg -c "/bin/vbash $(readlink -f $0) $@"
 fi
 
-run show interfaces > vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+INTERFACES+------+\n' > vyosConfig.txt
+run show interfaces >> vyosConfig.txt
+echo -e '\n+------+FIREWALL+------+\n' >> vyosConfig.txt
 run show firewall >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+RUNNING CONTAINERS+------+\n' >> vyosConfig.txt
 run show container >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+DOMAIN/OS INFO+------+\n' >> vyosConfig.txt
 run show host os >> vyosConfig.txt
 run show host domain >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+IP ROUTES+------+\n' >> vyosConfig.txt
 run show ip route >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+OPEN PORTS+------+\n' >> vyosConfig.txt
 run show ip ports >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+SRC/DEST NAT RULES+------+\n' >> vyosConfig.txt
 run show nat source rules >> vyosConfig.txt
 run show nat destination rules >> vyosConfig.txt
-cat '--------------------' >> vyosConfig.txt
+echo -e '\n+------+USERS/SESSIONS+------+\n' >> vyosConfig.txt
 run show users >> vyosConfig.txt
+echo -e '\n+------+------+------+\n' >> vyosConfig.txt
 
 exit
+echo -e 'Configuration details found in ./vyosConfig.txt'
