448252420145f30fbed111363f13db27ad53b639
SOC-SE <samuelbrucker296@gmail.com>
2025-11-23 11:15:10 -0600

    new temp fix

--- LinuxDev/tempFix.sh
+++ LinuxDev/tempFix.sh
@@ -1,11 +1,25 @@
-filebeat setup --index-management \
-  -E setup.template.json.enabled=true \
-  -E setup.template.json.path=/etc/filebeat/wazuh-template.json \
-  -E setup.template.json.name=wazuh \
-  -E setup.ilm.overwrite=true \
-  -E setup.ilm.enabled=false \
-  -E output.elasticsearch.hosts=["127.0.0.1:9200"] \
-  -E output.elasticsearch.protocol=https \
-  -E output.elasticsearch.username=admin \
-  -E output.elasticsearch.password=admin \
-  -E output.elasticsearch.ssl.verification_mode=none
+# 1. Generate hash for "Changeme1!"
+export JAVA_HOME=/usr/share/wazuh-indexer/jdk/
+/usr/share/wazuh-indexer/plugins/opensearch-security/tools/hash.sh -p "Changeme1!" > /tmp/hash_new.txt
+NEW_HASH=$(cat /tmp/hash_new.txt)
+
+# 2. Update admin password in internal_users.yml (Be specific to admin user)
+# We use a precise sed command to only target the admin user's hash
+sed -i "/^admin:/,/^  hash:/ s|hash:.*|hash: \"$NEW_HASH\"|" /etc/wazuh-indexer/opensearch-security/internal_users.yml
+
+# 3. Apply changes to the Database (The critical step)
+/usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh \
+  -cd /etc/wazuh-indexer/opensearch-security/ \
+  -nhnv \
+  -cacert /etc/wazuh-indexer/certs/root-ca.pem \
+  -cert /etc/wazuh-indexer/certs/admin.pem \
+  -key /etc/wazuh-indexer/certs/admin-key.pem \
+  -p 9200 \
+  -icl \
+  -h 127.0.0.1
+
+# 4. Update Dashboard Config to match
+sed -i 's/opensearch.password:.*/opensearch.password: Changeme1!/' /etc/wazuh-dashboard/opensearch_dashboards.yml
+
+# 5. Restart Dashboard
+systemctl restart wazuh-dashboard
